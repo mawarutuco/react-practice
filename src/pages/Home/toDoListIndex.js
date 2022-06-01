78897e059c7e109c86c6008e25f64d0c28cd8764
mawarutuco <xul4841268@gmail.com>
2022-06-01 22:28:46 +0800

    6/1 嘗試i18n

---
+++ src/pages/Home/toDoListIndex.js
@@ -0,0 +1,51 @@
+import { useState, useEffect, useRef } from "react";
+// import { API_GET_DATA } from "../../global/constants.js";
+
+import Edit from "./components/Edit.js";
+import List from "./components/List.js";
+
+
+// async function fetchData(setData) {
+//   const res = await fetch(API_GET_DATA);
+//   const { data } = await res.json();
+//   setData(data);
+// }
+
+// async function fetchSetData(data) {
+//   await fetch(API_GET_DATA, {
+//     method: "PUT",
+//     headers: { "Content-type": "application/json" },
+//     body: JSON.stringify({ data }),
+//   });
+// }
+
+const Home = () => {
+  const [data, setData] = useState([]);
+  const submittingStatus = useRef(false);
+
+  // useEffect(() => {
+  //   if (!submittingStatus) {
+  //     return;
+  //   }
+  //   fetchSetData(data).then((data) => (submittingStatus.current = false));
+  // }, [data]);
+
+  // useEffect(() => {
+  //   fetchData(setData);
+  // }, []);
+
+  return (
+    <div>
+      <h1>ToDoList</h1>
+      <Edit add={setData} submittingStatus={submittingStatus} />
+      <hr />
+      <List
+        listData={data}
+        deleteData={setData}
+        submittingStatus={submittingStatus}
+      />
+    </div>
+  );
+};
+
+export default Home;
