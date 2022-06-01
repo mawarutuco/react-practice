ac044e54149f4815d4b9acc4e9cdc53688826cac
mawarutuco <xul4841268@gmail.com>
2022-06-01 22:29:09 +0800

    6/1 練習 & 整理

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./pages/Home/index.js";
+import Home from "./pages/Home/toDoListIndex.js";
+// import { Circle } from "react-awesome-shapes";
 // import BootstrapTest from "./pages/Home/components/bootstrapTest";
 // import TDM from "./pages/Home/pixiTest/pixiTest.js";
+import "./pages/Home/i18nTest/i18n.js";
+import TestTranslation from "./pages/Home/i18nTest/testTranslation.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+   
     {/* <BootstrapTest /> */}
     {/* <TDM /> */}
+    
+    {/* <Circle
+      color="linear-gradient(135deg, red, white)"
+      size={["150px", "150px", "180px", "180px"]}
+      zIndex={-1}
+    />  */}
+    <Home />
+    <TestTranslation />
+    
   </React.StrictMode>
 );
 
