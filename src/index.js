3b6cc01d2344be6b8b9ef649788816b118455958
mawarutuco <xul4841268@gmail.com>
2022-06-01 23:31:48 +0800

    6/1 練習

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home/toDoListIndex.js";
-// import { Circle } from "react-awesome-shapes";
+import { Circle } from "react-awesome-shapes";
 // import BootstrapTest from "./pages/Home/components/bootstrapTest";
 // import TDM from "./pages/Home/pixiTest/pixiTest.js";
 import "./pages/Home/i18nTest/i18n.js";
@@ -15,11 +15,11 @@ root.render(
     {/* <BootstrapTest /> */}
     {/* <TDM /> */}
     
-    {/* <Circle
+    <Circle
       color="linear-gradient(135deg, red, white)"
       size={["150px", "150px", "180px", "180px"]}
       zIndex={-1}
-    />  */}
+    /> 
     <Home />
     <TestTranslation />
     
