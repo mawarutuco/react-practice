38effa0daa97a42dfbe208f47d5a638fdfaae083
mawarutuco <xul4841268@gmail.com>
2022-06-12 17:40:15 +0800

    任務 (保留未完成、進行中、已完成標籤

--- src/pages/Home/Task/TaskIndex.js
+++ src/pages/Home/Task/TaskIndex.js
@@ -1,50 +1,81 @@
 import React, { useState } from "react";
 import { Container, Button, Card } from "react-bootstrap";
 import swal from "sweetalert";
-import "./Task.css";
 
 const TaskIndex = () => {
   const [undone, setUndone] = useState([
-    { id: 1, title: "undone", content: "undone" },
-    { id: 2, title: "undone2", content: "undone2" },
+    { id: 1, title: "undone", content: "undone", type: "day" },
+    { id: 2, title: "undone2", content: "undone2", type: "week" },
   ]);
   const [ing, setIng] = useState([
-    { id: 3, title: "ing", content: "ing" },
-    { id: 4, title: "ing2", content: "ing2" },
+    { id: 3, title: "ing", content: "ing", type: "day" },
+    { id: 4, title: "ing2", content: "ing2", type: "week" },
   ]);
   const [done, setDone] = useState([
-    { id: 5, title: "done", content: "done" },
-    { id: 6, title: "done2", content: "done2" },
+    { id: 5, title: "done", content: "done", type: "day" },
+    { id: 6, title: "done2", content: "done2", type: "week" },
   ]);
 
   const Tasks = ({ page }) => {
-    return (
-      <Card border="dark">
-        {page === "undone" &&
-          undone.map((item) => (
+    const UndoneTasks = () => {
+      return undone.map((item) => (
+        <Card border="dark" key={item.id}>
+          <TaskItem item={item} />
+          <Card.Footer>{TaskUndoneBtn(item)}</Card.Footer>
+        </Card>
+      ));
+    };
+
+    const IngTasks = () => {
+      return ing.map((item) => (
+        <Card border="dark" key={item.id}>
+          <TaskItem item={item} />
+          <Card.Footer>{TaskIngBtn(item)}</Card.Footer>
+        </Card>
+      ));
+    };
+
+    const DoneTasks = () => {
+      return done.map((item) => (
+        <Card border="dark" key={item.id}>
+          <TaskItem item={item} />
+        </Card>
+      ));
+    };
+
+    const 顯示任務 = () => {
+      const all = [...undone, ...ing, ...done];
+      switch (page) {
+        case "undone":
+          return <UndoneTasks />;
+
+        case "ing":
+          return <IngTasks />;
+
+        case "done":
+          return <DoneTasks />;
+
+        case "day": {
+          return all.map((item) => (
             <Card border="dark" key={item.id}>
-              <TaskItem item={item} page={page} />
-              <Card.Footer>{TaskUndoneBtn(item)}</Card.Footer>
+              {item.type === "day" && <TaskItem item={item} />}
             </Card>
-          ))}
-        {page === "ing" &&
-          ing.map((item) => (
+          ));
+        }
+
+        case "week": {
+          return all.map((item) => (
             <Card border="dark" key={item.id}>
-              <TaskItem item={item} page={page} />
-              <Card.Footer>{TaskIngBtn(item)}</Card.Footer>
+              {item.type === "week" && <TaskItem item={item} />}
             </Card>
-          ))}
-        {page === "done" &&
-          done.map((item) => (
-            <Card border="dark" key={item.id}>
-              <TaskItem item={item} page={page} />
-            </Card>
-          ))}
-      </Card>
-    );
+          ));
+        }
+      }
+    };
+    return <Card border="dark">{顯示任務()}</Card>;
   };
 
-  const 移轉任務所在地 = (從這裡, 到那裏, item) => {
+  const 移轉任務 = (從這裡, 到那裏, item) => {
     //從這裡 刪除
     從這裡((prev) => prev.filter((el) => el.id != item.id));
     //新增 到那裏
@@ -52,35 +83,53 @@ const TaskIndex = () => {
   };
 
   const TaskUndoneBtn = (item) => {
+    const 自動翻頁 = () => {
+      if (undone.length === 1) setPage("ing");
+    };
+
     return (
-      <Button onClick={() => 移轉任務所在地(setUndone, setIng, item)}>
+      <Button
+        onClick={() => {
+          移轉任務(setUndone, setIng, item);
+          自動翻頁();
+        }}
+      >
         領取任務
       </Button>
     );
   };
 
   const TaskIngBtn = (item) => {
-    const [可否領獎勵, 設定可否領獎勵] = useState(true);
+    const [可否領獎勵, 設定可否領獎勵] = useState(false);
+
+    const 完成某個東西 = () => 設定可否領獎勵(true);
+
     return (
-      <Button
-        disabled={可否領獎勵 ? false : true}
-        onClick={() => {
-          移轉任務所在地(setIng, setDone, item);
-          swal("獲得獎勵，XXX元", { icon: "success" });
-        }}
-      >
-        {可否領獎勵 ? "領取獎勵" : "尚未完成"}
-      </Button>
+      <>
+        <Button
+          disabled={可否領獎勵 ? false : true}
+          onClick={() => {
+            移轉任務(setIng, setDone, item);
+            swal("獲得獎勵，XXX元", { icon: "success" });
+          }}
+        >
+          {可否領獎勵 ? "領取獎勵" : "尚未完成"}
+        </Button>
+        <Button variant="outline-secondary" onClick={() => 完成某個東西()}>
+          完成某個東西
+        </Button>
+      </>
     );
   };
 
-  const TaskItem = ({ item, page }) => {
-    const { title, content } = item;
+  const TaskItem = ({ item }) => {
+    const { title, content, type } = item;
 
     return (
       <>
         <Card.Header>
           <h3>{title}</h3>
+          <span>{type}</span>
         </Card.Header>
         <Card.Body>
           <p>{content}</p>
@@ -93,11 +142,11 @@ const TaskIndex = () => {
 
   return (
     <Container>
-      <Button onClick={() => console.log("完成某個東西")}>完成某個東西</Button>
-      <hr />
       <Button onClick={() => setPage("undone")}>未完成</Button>
       <Button onClick={() => setPage("ing")}>進行中</Button>
       <Button onClick={() => setPage("done")}>已完成</Button>
+      <Button onClick={() => setPage("day")}>day</Button>
+      <Button onClick={() => setPage("week")}>week</Button>
       <Tasks page={page} />
     </Container>
   );
